df17358be873223a0fc8cf67d93adba6ae518f77
q7df1 <2567176308@qq.com>
2025-09-26 15:25:55 +0800

    add other stream protocol && ui launcher logo

---
+++ app/src/main/java/com/android/v2rayForAndroidUI/model/stream/KcpSettings.kt
@@ -0,0 +1,18 @@
+package com.android.v2rayForAndroidUI.model.stream
+
+data class KcpSettings(
+    val mtu: Int = 1350,
+    val tti: Int = 20,
+    val uplinkCapacity: Int = 5,
+    val downlinkCapacity: Int = 20,
+    val congestion: Boolean = false,
+    val readBufferSize: Int = 1,
+    val writeBufferSize: Int = 1,
+    val header: KcpHeaderObject,
+    val seed: String? = null
+)
+
+data class KcpHeaderObject(
+    val type: String? = null,
+    val domain: String? = null
+)
