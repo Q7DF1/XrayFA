efe179ae35edba0320f13373a624a88c0e2287a1
q7df1 <2567176308@qq.com>
2025-12-29 13:41:11 +0800

    issue fixed: Text buffer lower boundary #128

--- app/src/main/java/com/android/xrayfa/ui/QRCodeActivity.kt
+++ app/src/main/java/com/android/xrayfa/ui/QRCodeActivity.kt
@@ -1,7 +1,21 @@
 package com.android.xrayfa.ui
 
+import android.os.Bundle
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import com.journeyapps.barcodescanner.CaptureActivity
 import javax.inject.Inject
 
 class QRCodeActivity@Inject constructor(): CaptureActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView.findViewById(android.R.id.content)) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(bottom = insets.bottom)
+            windowInsets
+        }
+    }
 }
